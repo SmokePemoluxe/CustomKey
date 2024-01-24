b37a5761906c5a9d49eeac45145c79509f76327e
SmokePemoluxe <alexey1999bittok@gmail.com>
2024-01-24 19:24:55 +0700

    adfgdafgagf

--- CustomKey Programm/ProgrammUpdater.bat
+++ CustomKey Programm/ProgrammUpdater.bat
@@ -1,19 +1,17 @@
 @echo off
 setlocal enabledelayedexpansion
 
-rem Устанавливаем путь к папке update
 set "sourceFolder=update"
 
 set "customKeyPath=%scriptDir%CustomKey.exe"
 
-taskkill /F /IM CustomKey.exe 2>nul
-timeout /t 1 >nul
-rem Копируем все файлы из папки update в текущую директорию с заменой
+timeout /t 2 /nobreak > nul
+taskkill /F /IM CustomKey.exe 3>nul
+timeout /t 2 /nobreak > nul
 xcopy /s /y "%sourceFolder%\*" "%CD%"
-timeout /t 1 >nul
-rem Удаляем папку update
+timeout /t 2 /nobreak > nul
 rd /s /q "%sourceFolder%"
-timeout /t 1 >nul
+timeout /t 2 /nobreak > nul
 start "" "%customKeyPath%"
-timeout /t 1 >nul
+timeout /t 2 /nobreak > nul
 exit
